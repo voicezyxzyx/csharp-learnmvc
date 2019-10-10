b10976362c974dae367e15dbab99e0469345df40
voicezyxzyx <450538542@qq.com>
2019-10-10 11:21:40 +0800

    change

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Student
+    {
+        public int sno;
+        public string sname;
+        public string ssex;
+        public string saddr;
+        public string phone;
+    }
+}
